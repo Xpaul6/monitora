00760ebe1db72acf8a5fa4f63ab76b08646609b1
Pavel Kholkin <xpaul6@gmail.com>
2026-04-17 14:39:08 +0500

    fix: raw log model field property to fix migration incompatbility

--- backend/Models/raw_logs.go
+++ backend/Models/raw_logs.go
@@ -8,7 +8,7 @@ type RawLog struct {
 	ComponentID  uint      `json:"component_id"`
 	MetricTypeID uint      `json:"metric_type_id"`
 	Value        float64   `json:"value"`
-	Timestamp    time.Time `json:"timestamp"`
+	Timestamp    time.Time `json:"timestamp" gorm:"not null"`
 
 	Component  Component  `gorm:"foreignKey:ComponentID"`
 	MetricType MetricType `gorm:"foreignKey:MetricTypeID"`
