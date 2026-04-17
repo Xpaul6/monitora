e3b0bddf94502e148e74800fb5f3c393f2e17c51
Pavel Kholkin <xpaul6@gmail.com>
2026-04-17 14:44:06 +0500

    feat: authentication utils & models

---
+++ backend/utils/auth/passwords.go
@@ -0,0 +1,15 @@
+package authutils
+
+import "golang.org/x/crypto/bcrypt"
+
+func HashPassword(password string) (string, error) {
+	res, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(res), nil
+}
+
+func VerificatePassword(hash string, pass string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(pass))
+}
