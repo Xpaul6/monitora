255314f63cf0145d163f6f4d06561570915e0b60
Pavel Kholkin <xpaul6@gmail.com>
2026-04-29 22:34:21 +0500

    svelte migration

---
+++ frontend/src/App.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+  let status = $state<string>("offline")
+
+  async function ping() {
+    try {
+      const res = await fetch("/api/ping");
+      if (!res.ok) throw new Error("lol no");
+
+      status = "online";
+    } catch (e) {
+      console.log(e);
+      status = "offline";
+    }
+  }
+
+  onMount(ping)
+</script>
+
+<div>
+  <h1>monitora</h1>
+  <h3>Backend status: { status }</h3>
+</div>
