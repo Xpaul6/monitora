8c73d934c689600db083c2fddfacd307aea4bd01
Pavel Kholkin <xpaul6@gmail.com>
2026-05-05 00:27:11 +0500

    wip: single chart

--- frontend/src/components/ServerDashboard.svelte
+++ frontend/src/components/ServerDashboard.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import { Chart, registerables } from "chart.js";
+  import "chartjs-adapter-date-fns";
+  Chart.register(...registerables);
+
   import {
     getMetricTypes,
     getServerComponents,
@@ -20,22 +24,46 @@
   let periodBegin = $state<Date>({} as Date);
   let periodEnd = $state<Date>({} as Date);
   let stats = $state<GetStatsByPeriodResponse[]>([]);
-
-  let serverComponentMap = $derived.by<Map<number, Component>>(() => {
-    let ret = new Map<number, Component>();
-    for (let c of serverComponents) {
-      ret.set(c.ID, c);
-    }
-    return ret;
+  let canvasElement = $state<any>();
+  let chartInstance = $state<any>();
+  let chartData = $derived({
+    datasets: [
+      {
+        label: stats[0].metric_type.name,
+        data: stats
+          .filter((m) => m.metric_type.name == "cpu_load")
+          .map((m) => ({
+            x: m.timestamp,
+            y: m.value,
+          })),
+        borderColor: getRandomColor(),
+        tension: 0.1,
+      },
+    ],
   });
 
-  let metricTypeMap = $derived.by<Map<number, MetricType>>(() => {
-    let ret = new Map<number, MetricType>();
-    for (let m of metricTypes) {
-      ret.set(m.ID, m);
-    }
-    return ret;
-  });
+  // let serverComponentMap = $derived.by<Map<number, Component>>(() => {
+  //   let ret = new Map<number, Component>();
+  //   for (let c of serverComponents) {
+  //     ret.set(c.ID, c);
+  //   }
+  //   return ret;
+  // });
+
+  // let metricTypeMap = $derived.by<Map<number, MetricType>>(() => {
+  //   let ret = new Map<number, MetricType>();
+  //   for (let m of metricTypes) {
+  //     ret.set(m.ID, m);
+  //   }
+  //   return ret;
+  // });
+
+  function getRandomColor() {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
+  }
 
   async function handleGetServerComponents() {
     const body: GetServerComponentsRequest = { id: server.ID };
@@ -80,16 +108,44 @@
     } catch (e) {
       alert("Network error: " + e);
     }
+
+    drawCharts();
+  }
+
+  function drawCharts() {
+    if (canvasElement && !chartInstance) {
+      // Initialize Chart
+      chartInstance = new Chart(canvasElement, {
+        type: "line",
+        data: chartData,
+        options: {
+          responsive: true,
+          scales: {
+            x: {
+              type: "time", // Sets X-axis to handle time
+              time: { unit: "minute" },
+              title: { display: true, text: "Time" },
+            },
+            y: {
+              title: {
+                display: true,
+                text: stats[0]?.metric_type.unit || "Value",
+              },
+            },
+          },
+        },
+      });
+    }
   }
 
   onMount(handleGetServerComponents);
   onMount(handleGetMetricTypes);
 </script>
 
-<div>
+<div class="flex flex-col items-center">
   <h1>Dashboard</h1>
   <!-- Server info -->
-  <div class="flex flex-row gap-5">
+  <div class="flex flex-row gap-5 justify-around">
     <div>
       <h2>Server:</h2>
       <p>ID: {server.ID}</p>
@@ -119,12 +175,15 @@
       />
       <label for="end" class="flex items-center">Period end</label>
       <input type="date" id="end" class="form-input" bind:value={periodEnd} />
-      <div class="flex flex-row justify-around mt-3">
+      <div class="flex items-center">
         <button class="form-button" onclick={handleGetStatsByPeriod}
           >Get metrics</button
         >
       </div>
     </form>
+  </div>
+  <div class="chart-container">
+    <canvas bind:this={canvasElement}></canvas>
   </div>
 </div>
 
